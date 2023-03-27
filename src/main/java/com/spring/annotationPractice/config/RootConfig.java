11d81d9e8df46ee885cbefca6bd422afdf580378
96mr <exmerang@gmail.com>
2023-03-27 17:57:10 +0900

    add .gitignore

---
+++ src/main/java/com/spring/annotationPractice/config/RootConfig.java
@@ -0,0 +1,16 @@
+package com.spring.annotationPractice.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+	/*
+	 * @Bean public DataSource dataSource() { DriverManagerDataSource dataSource =
+	 * new DriverManagerDataSource();
+	 * 
+	 * return }
+	 */
+}
